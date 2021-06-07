10040ae773d3bd68ea9bd56fb0155f9ec9ce485d
luis-estena21 <luisnugo@gmail.com>
2021-06-07 08:31:56 -0400

    Se agregan imagenes

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ ham.addEventListener('click', () => {
     barras.forEach(child => {child.classList.toggle('animado')});
     ham.classList.toggle('girar');
 });
+
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('La Capital del Sol')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
