4a0c7b9aa1c703996c7f10295e03ee1cf5265e86
luis-estena21 <luisnugo@gmail.com>
2021-06-07 16:06:37 -0400

    Version 6

--- js/script.js
+++ js/script.js
@@ -9,17 +9,3 @@ ham.addEventListener('click', () => {
     barras.forEach(child => {child.classList.toggle('animado')});
     ham.classList.toggle('girar');
 });
-
-let app = document.getElementById('typewriter');
- 
-let typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
-});
- 
-typewriter
-  .pauseFor(2500)
-  .typeString('La Capital del Sol')
-  .pauseFor(200)
-  .deleteChars(10)
-  .start();
